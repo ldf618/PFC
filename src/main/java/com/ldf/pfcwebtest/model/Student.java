bdf8d63cd400693cb7b0d4d4cd88cb48ab80a37f
Lo <Lo@DESKTOP-713KK4T>
2022-02-20 12:43:38 +0100

    Add files via upload

--- src/main/java/com/ldf/pfcwebtest/model/Student.java
+++ src/main/java/com/ldf/pfcwebtest/model/Student.java
@@ -1,12 +1,17 @@
 package com.ldf.pfcwebtest.model;
 
+import javax.persistence.*;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+//javax.persistence
+@Entity
+
+//Lombok
 @NoArgsConstructor
 public class Student extends User {
-     
-    @Builder(builderMethodName = "studentBuilder")   
+
+    @Builder(builderMethodName = "studentBuilder")
     public Student(int idUser, String name, String surname1, String surname2, String dni, String userName, String userPassword) {
         super(idUser, name, surname1, surname2, dni, userName, userPassword);
     }
--- src/main/java/com/ldf/pfcwebtest/model/User.java
+++ src/main/java/com/ldf/pfcwebtest/model/User.java
@@ -1,22 +1,55 @@
 package com.ldf.pfcwebtest.model;
 
+import javax.persistence.*;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+//javax.persistence
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+
+//Lombok
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class User {
-	
+	@Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int idUser;
+        
+        @Column
+        @NotNull
+        @Size(min = 2, max = 50)
 	private String name;
+        
+        @Column
+        @Size(max = 50)
 	private String surname1;
+        
+        @Column
+        @Size(max = 50)
 	private String surname2;
+        
+        @Column
+        @NotNull
+        @Size(min = 9, max = 9)
 	private String dni;
+        
+        @Column
+        @NotNull
+        @Size(min = 2, max = 50)
 	private String userName;
+        
+        @Column
+        @NotNull
+        @Size(min = 2, max = 50)
 	private String userPassword;
  
 }
